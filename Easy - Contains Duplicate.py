af93f3a45cf4fa5ad24a9a5e6d9f23aa9037ce6b
shaarvin <domunshaarvin@gmail.com>
2024-03-19 14:03:09 +0400

    2024 Prep

--- Easy - Contains Duplicate.py
+++
@@ -1,6 +0,0 @@
-def solulu(nums):
-    seen = set()
-    for num in nums:
-        if num in seen: return True
-        else: seen.add(num)
-    return False
--- Easy - Single number.py
+++
@@ -1,9 +0,0 @@
-def solulu(nums):
-    result = nums[0]
-    for i in range(1,len(nums)):
-        result ^= nums[i]
-
-    return result
-
-
-print(solulu([1,1,2,2,3,3,4]))
